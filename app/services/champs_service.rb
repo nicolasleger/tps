c182b2b3c15507dea9d9eac43fef354a06806a76
Simon Lehericey <simon.lehericey@gmail.com>
2017-03-29 13:37:07 +0200

    ChampsService: add tests and refactor

--- app/services/champs_service.rb
+++ app/services/champs_service.rb
@@ -1,27 +1,35 @@
 class ChampsService
-  def self.save_formulaire champs, params, check_mandatory=true
-    errors = Array.new
+  class << self
+    def save_formulaire(champs, params, check_mandatory = true)
+      champs.select { |c| c.type_champ != 'datetime' }
+            .each { |c| c.value = params[:champs]["'#{c.id}'"] }
 
-    champs.each do |champ|
-      champ.value = params[:champs]["'#{champ.id}'"]
+      champs.select { |c| c.type_champ == 'datetime' }
+            .each { |c| c.value = parse_datetime(c.id, params) }
 
-      if champ.type_champ == 'datetime'
-        champ.value = params[:champs]["'#{champ.id}'"]+
-            ' ' +
-            params[:time_hour]["'#{champ.id}'"] +
-            ':' +
-            params[:time_minute]["'#{champ.id}'"]
-      end
+      champs.select(&:changed?).each(&:save)
 
-      if check_mandatory
-        if champ.mandatory? && (champ.value.nil? || champ.value.blank?)
-          errors.push({message: "Le champ #{champ.libelle} doit être rempli."})
-        end
-      end
-
-      champ.save if champ.changed?
+      return [] unless check_mandatory
+      champs.select(&:mandatory_and_blank?)
+            .map { |c| build_error_message_for(c) }
     end
 
-    errors
+    private
+
+    def parse_datetime(champ_id, h)
+      "#{h[:champs]["'#{champ_id}'"]} #{extract_hour(champ_id, h)}:#{extract_minute(champ_id, h)}"
+    end
+
+    def extract_hour(champ_id, h)
+      h[:time_hour]["'#{champ_id}'"]
+    end
+
+    def extract_minute(champ_id, h)
+      h[:time_minute]["'#{champ_id}'"]
+    end
+
+    def build_error_message_for(champ)
+      { message: "Le champ #{champ.libelle} doit être rempli." }
+    end
   end
 end
